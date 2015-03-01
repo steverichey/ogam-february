6c7a1771893cbcb9c12ff983bed7859156c1923f
Steve Richey <srichey@floatlearning.com>
2015-03-01 15:18:19 -0600

    correct render position errors due to OOO mistake

--- android/assets/data/simple-vertex.glsl
+++ android/assets/data/simple-vertex.glsl
@@ -9,5 +9,5 @@ varying vec2 v_texCoord0;
 
 void main() {
     v_texCoord0 = a_texCoord0;
-    gl_Position = u_worldTrans * u_projTrans * vec4(a_position, 1.0);
+    gl_Position = u_projTrans * u_worldTrans * vec4(a_position, 1.0);
 }
