67796119716e56da5e169086944c14f69af1f1c1
Steve Richey <srichey@floatlearning.com>
2015-03-01 15:01:19 -0600

    fix stuff

--- core/src/com/steverichey/ogamfebruary/GameClass.java
+++ core/src/com/steverichey/ogamfebruary/GameClass.java
@@ -3,42 +3,31 @@ package com.steverichey.ogamfebruary;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
-import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.Shader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.utils.Array;
 
 public class GameClass extends ApplicationAdapter implements ApplicationListener {
 	private PerspectiveCamera camera;
-    private ModelBatch modelBatch;
-    private Environment environment;
     private CameraInputController cameraInputController;
-    private AssetManager assetManager;
-    private Boolean loading = false;
-    private Array<ModelInstance> instances = new Array<ModelInstance>();
-    private Array<ModelInstance> blocks = new Array<ModelInstance>();
-    private Array<ModelInstance> invaders = new Array<ModelInstance>();
-    private ModelInstance ship;
-    private ModelInstance space;
+    private Array<ModelInstance> instances;
+    private Model model;
+    private Shader shader;
+    private ModelBatch modelBatch;
 
     @Override
 	public void create() {
-        modelBatch = new ModelBatch();
-
-        environment = new Environment();
-        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
-
+        // camera setup
 		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.position.set(0f, 7f, 10f);
+        camera.position.set(0f, 8f, 8f);
         camera.lookAt(0f, 0f, 0f);
         camera.near = 1f;
         camera.far = 300f;
@@ -47,60 +36,35 @@ public class GameClass extends ApplicationAdapter implements ApplicationListener
         cameraInputController = new CameraInputController(camera);
         Gdx.input.setInputProcessor(cameraInputController);
 
-        assetManager = new AssetManager();
-        assetManager.load("data/ship.obj", Model.class);
-        assetManager.load("data/block.obj", Model.class);
-        assetManager.load("data/invader.obj", Model.class);
-        assetManager.load("data/spacesphere.obj", Model.class);
-        assetManager.load("data/main.g3dj", Model.class);
+        // begin model loading
+        instances = new Array<ModelInstance>();
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createSphere(2f, 2f, 2f, 20, 20, new Material(), Usage.Position | Usage.Normal | Usage.TextureCoordinates);
 
-        loading = true;
-	}
-
-    private void doneLoading() {
-        ship = new ModelInstance(assetManager.get("data/ship.obj", Model.class));
-        ship.transform.setToRotation(Vector3.Y, 180).trn(0, 0, 6f);
-        instances.add(ship);
-
-        Model gameScene = assetManager.get("data/main.g3dj", Model.class);
-        for (float x = -5f; x <= 5f; x +=2f) {
-            ModelInstance block = new ModelInstance(gameScene, "block");
-            block.transform.setToTranslation(x, 0, 3f);
-            instances.add(block);
-            blocks.add(block);
-        }
-
-        Model invaderModel = assetManager.get("data/invader.obj", Model.class);
-        for (float x = -5f; x <= 5f; x += 2f) {
-            for (float z = -8f; z <= 0f; z += 2f) {
-                ModelInstance invader = new ModelInstance(invaderModel);
-                invader.transform.setToTranslation(x, 0, z);
-                instances.add(invader);
-                invaders.add(invader);
+        for (int x = -5; x <= 5; x += 2) {
+            for (int z = -5; z <= 5; z += 2) {
+                ModelInstance modelInstance = new ModelInstance(model, x, 0, z);
+                instances.add(modelInstance);
             }
         }
 
-        space = new ModelInstance(assetManager.get("data/spacesphere.obj", Model.class));
+        shader = new SimpleShader();
+        shader.init();
 
-        loading = false;
-    }
+        modelBatch = new ModelBatch();
+	}
 
 	@Override
 	public void render() {
-        if (loading && assetManager.update()) {
-            doneLoading();
-        }
-
-		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
         cameraInputController.update();
 
-        modelBatch.begin(camera);
-        modelBatch.render(instances, environment);
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        if (space != null) {
-            modelBatch.render(space, environment);
+        modelBatch.begin(camera);
+
+        for (ModelInstance instance : instances) {
+            modelBatch.render(instance, shader);
         }
 
         modelBatch.end();
@@ -108,8 +72,12 @@ public class GameClass extends ApplicationAdapter implements ApplicationListener
 
     @Override
     public void dispose() {
+        shader.dispose();
+        model.dispose();
         modelBatch.dispose();
-        instances.clear();
-        assetManager.dispose();
     }
+
+    @Override public void resume () {}
+    @Override public void resize (int width, int height) {}
+    @Override public void pause () {}
 }
---
+++ core/src/com/steverichey/ogamfebruary/SimpleShader.java
@@ -0,0 +1,72 @@
+package com.steverichey.ogamfebruary;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.graphics.g3d.Shader;
+import com.badlogic.gdx.graphics.g3d.utils.RenderContext;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+public class SimpleShader implements Shader {
+    private ShaderProgram shaderProgram;
+    private Camera camera;
+    private RenderContext renderContext;
+    private int u_projTrans;
+    private int u_worldTrans;
+
+    @Override
+    public void init() {
+        String vert = Gdx.files.internal("data/simple-vertex.glsl").readString();
+        String frag = Gdx.files.internal("data/simple-fragment.glsl").readString();
+        shaderProgram = new ShaderProgram(vert, frag);
+
+        if (!shaderProgram.isCompiled()) {
+            throw new GdxRuntimeException(shaderProgram.getLog());
+        }
+
+        u_projTrans = shaderProgram.getUniformLocation(PROJ_TRANS);
+        u_worldTrans = shaderProgram.getUniformLocation(WORLD_TRANS);
+    }
+
+    @Override
+    public void dispose() {
+        shaderProgram.dispose();
+    }
+
+    @Override
+    public void begin(Camera camera, RenderContext context) {
+        this.camera = camera;
+        this.renderContext = context;
+
+        shaderProgram.begin();
+        shaderProgram.setUniformMatrix(u_projTrans, camera.combined);
+        renderContext.setDepthTest(GL20.GL_LEQUAL);
+        renderContext.setCullFace(GL20.GL_BACK);
+    }
+
+    @Override
+    public void render(Renderable renderable) {
+        shaderProgram.setUniformMatrix(u_worldTrans, renderable.worldTransform);
+        renderable.mesh.render(shaderProgram, renderable.primitiveType, renderable.meshPartOffset, renderable.meshPartSize);
+    }
+
+    @Override
+    public void end() {
+        shaderProgram.end();
+    }
+
+    @Override
+    public int compareTo(Shader shader) {
+        return 0;
+    }
+
+    @Override
+    public boolean canRender(Renderable instance) {
+        return true;
+    }
+
+    private final String PROJ_TRANS = "u_projTrans";
+    private final String WORLD_TRANS = "u_projTrans";
+}
